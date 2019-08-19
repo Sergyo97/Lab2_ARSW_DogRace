67f008cd91fb211f05619af6d992a9aed64ed564
urobs17 <michael.preciado@mail.escuelaing.edu.co>
2019-08-19 18:34:21 -0500

    keyListener

--- Part_1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ Part_1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,54 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.*;
+
+import javax.swing.JPanel;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
+		boolean EnterPressed=false;
+		List<PrimeFinderThread> thList = new ArrayList<PrimeFinderThread>();
+		thList.add(new PrimeFinderThread(0, 10000000));
+		thList.add(new PrimeFinderThread(10000000, 20000000));
+		thList.add(new PrimeFinderThread(20000000, 30000000));
+		for (PrimeFinderThread pft : thList)
+			pft.start();
+		Thread t=new Thread() {
+			public void run() {
+				try {
+					Thread.currentThread().sleep(5000);
+					System.out.println("Han pasado 5 segundos");
+					thList.wait();
+				} catch (Exception e) {
+
+				}
+			}
+		};
+		t.start();
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		JPanel contentPane = new JPanel();
+		contentPane.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					thList.notify();
+					Thread.currentThread().notify();
+				}
+			}
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+		});
+		while(!EnterPressed){
+
+		}
 	}
-	
 }
